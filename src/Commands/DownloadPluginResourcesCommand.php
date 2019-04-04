49075b437cfc77be2d046570e9de4ed5dc0e9918
Dwayne Sharp <dwayne.sharp@4fb.de>
2019-04-04 17:26:04 +0200

    improved resource download with new feature

--- src/Commands/DownloadPluginResourcesCommand.php
+++ src/Commands/DownloadPluginResourcesCommand.php
@@ -2,9 +2,9 @@
 
 namespace FroshPluginUploader\Commands;
 
-use FroshPluginUploader\Components\PluginUpdater;
 use FroshPluginUploader\Components\ResourcesDownloader;
 use FroshPluginUploader\Components\Util;
+use FroshPluginUploader\Structs\Config\PluginConfig;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -22,16 +22,23 @@ class DownloadPluginResourcesCommand extends Command implements ContainerAwareIn
         $this
             ->setName('plugin:download:resources')
             ->setDescription('Downloads the resources from account to given folder. Needed for plugin:upload')
-            ->addArgument('path', InputArgument::REQUIRED, 'Path to /Resources/store folder');
+            ->addArgument('path', InputArgument::OPTIONAL, 'Path to /Resources/store folder');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (!Util::getEnv('PLUGIN_ID')) {
-            throw new \RuntimeException('The enviroment variable $PLUGIN_ID is required');
-        }
+        $plugins = Util::getPluginConfigs(true);
 
-        $this->container->get(ResourcesDownloader::class)->download($input->getArgument('path'));
+        /**
+         * @var PluginConfig $plugin
+         */
+        foreach ($plugins as $plugin)
+        {
+            $p = $input->getArgument('path');
+            $path = $p ? $p: $plugin->path . '/' . $plugin->name;
+            putenv("PLUGIN_ID=" . $plugin->id);
+            $this->container->get(ResourcesDownloader::class)->download($path . '/Resources/store');
+        }
 
         $io = new SymfonyStyle($input, $output);
         $io->success('Downloaded store data to given folder');
